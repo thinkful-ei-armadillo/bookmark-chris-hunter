f78dea7fa5b9aea80965675c9ca78232d98c583e
Christopher <christopher.carnivale@gmail.com>
2019-01-30 18:10:10 -0500

    cleaned up

--- scripts/bookmarkList.js
+++ scripts/bookmarkList.js
@@ -3,30 +3,31 @@
 
 // eslint-disable-next-line no-unused-vars
 const bookmarkList = (function (){
+  let add = false; 
 
-  function renderBaseTopPanel () {
+  function generateBaseTopPanel () {
     return `
     <button class="js-add-new-button" type="button">
     Add New Bookmark
   </button>`;
   }
 
-  function renderAddNewTopPanel () {
+  function generateAddNewTopPanel () {
     return `
     <form class="js-add-item">
           <label for="title">Title</label>
-          <input type="text" class="js-title-input">
+          <input type="text" value="Hats" class="js-title-input">
           <label for="link">Link</label>
-          <input type="text" class="js-link-input">
+          <input type="url" value="http://www.hats.com" class="js-link-input">
           <label for="description">Description</label>
-          <input type="text" class="js-description-input">
+          <input type="text" value="Hats" class="js-description-input">
           <label for="rating">Rating</label>
-          <input type="text" class="js-rating-input">
+          <input type="number" value="4" min="1" max="5" class="js-rating-input">
           <input type="submit" class="js-create-bookmark">
         </form>`;
   }
 
-  function renderBaseMiddlePanel () {
+  function generateMiddlePanel () {
     return `
     <form class="js-filter-bookmarks">
       <select name="filter">
@@ -55,34 +56,13 @@ const bookmarkList = (function (){
     </ul>`;
   }
 
-  function generateBookmarkItemsString(bookmarkList){
-    const items = bookmarkList.map((item) => {
-      if (item.collapse === false) {
-        return generateItemElement(item);
-      }
-      else {
-        return renderCollapseBottomPanel(item);
-      }
-    });
-    return items.join('');
-  }
 
-  function renderBaseBottomPanel () {
-    let items = [...store.items];
-
-    const bookmarkListItemsString = generateBookmarkItemsString(items); 
-
-
-    $('.js-bottom-panel').html(bookmarkListItemsString); 
-    
-  }
-
-  function renderCollapseBottomPanel (item) {
+  function generateItemExpandedElement (item) {
     return `
     <ul class="bookmarks">
       <li class="bookmark" data-item-id=${item.id}>
         Rating ${item.rating} Title ${item.title}
-        Description ${item.description} URL ${item.url}
+        Description ${item.desc} URL <a href="${item.url}">${item.url}</a>
         <button class="js-collapse" type="button">Details</button>
         <button class= "js-edit" type="button">Edit</button>
         <label for="delete">
@@ -92,24 +72,53 @@ const bookmarkList = (function (){
     </ul>`;
   }
 
-  function renderEditBottomPanel () {
+
+  function generateBookmarkItemsString(bookmarkList){
+    const items = bookmarkList.map((item) => {
+      if(item.editing)
+      {
+        return generateEditItem(item); 
+      }
+      else if (item.collapse === false) {
+        return generateItemElement(item);
+      }
+      else {
+        return generateItemExpandedElement(item);
+      }
+    });
+    return items.join('');
+  }
+
+  function render() {
+    let items = [...store.items];
+    const bookmarkListItemsString = generateBookmarkItemsString(items);
+    if(add){
+      $('.js-top-panel').html(generateAddNewTopPanel);
+    } 
+    else{
+      $('.js-top-panel').html(generateBaseTopPanel); 
+    }
+    $('.js-middle-panel').html(generateMiddlePanel);
+    $('.js-bottom-panel').html(bookmarkListItemsString); 
+  }
+
+  function generateEditItem(item) {
     return `
     <ul class="bookmarks">
     <li class="bookmark" data-item-id=${item.id}>
       <form for="edit">
         <label for="rating">Rating</label>
-        <input type="text" placeholder=${item.rating}>
+        <input type="number" value=${item.rating}>
         <label for="title">Title</label>
-        <input type="text" placeholder=${item.title}>
+        <input type="text" value=${item.title}>
         <div class="js-desciption">
           <label for="description">Description</label>
-          <input type="text" placeholder=${item.description}>
+          <input type="text" value=${item.desc}>
           <label for="link">Link</label>
-          <input type="text" placeholder=${item.link}>
+          <input type="url" value=${item.url}>
         </div>
       </form>
-      <button class="js-collapse" type="button">Details</button>
-      <button class= "js-edit" type="button">Edit</button>
+      <button class= "js-edit" type="button">Nevermind</button>
       <label for="delete">
         <input type="submit" value="Delete" class="js-delete">
       </label>  
@@ -129,17 +138,25 @@ const bookmarkList = (function (){
       api.deleteItem(id)
         .then(()=> {
           store.findAndDelete(id); 
-          render();
-        }); 
-    });
+          render();}); 
+    });  
   }
 
-  function handleCollapseItemClicked() {
+  function handleCollapseItemClicked(){
     $('.js-bottom-panel').on('click', '.js-collapse', (event) =>{
       const id = getItemIdFromElement(event.currentTarget);
       const item = store.findById(id);
       store.findAndUpdate(item, {collapse: !item.collapse});
-      renderBaseBottomPanel(item);
+      render();
+    });
+  }
+
+  function handleEditItemClicked() {
+    $('.js-bottom-panel').on('click', '.js-edit', (event) =>{
+      const id = getItemIdFromElement(event.currentTarget);
+      const item = store.findById(id);
+      store.findAndUpdate(item, {editing: !item.editing});
+      render();
     });
   }
 
@@ -167,37 +184,31 @@ const bookmarkList = (function (){
           return response.json();})
         .then((responseJson)=>{
           store.addItem(responseJson);
+          add = false; 
           render();   
         }); 
     });
   }
 
-  function render() {
-    $('.js-top-panel').html(renderBaseTopPanel);
-    $('.js-middle-panel').html(renderBaseMiddlePanel);
-    $('.js-bottom-panel').html(renderBaseBottomPanel);
-    bindEventListeners(); 
-  }
-
-  function handleNewItemButton() {
-    $('.js-add-new-button').on('click', () => {
-      $('.js-top-panel').html(renderAddNewTopPanel);
-      $('.js-middle-panel').html(renderBaseMiddlePanel);
-      $('.js-bottom-panel').html(renderBaseBottomPanel);
+  function handleNewItemClicked() {
+    $('.js-top-panel').on('click', '.js-add-new-button', () => {
+      add = true;
+      render();  
     });
   }
 
   function bindEventListeners() {
-    handleNewItemButton();
+    handleNewItemClicked();
     handleNewItemSubmit();
     handleDeleteItemClicked();
     handleCollapseItemClicked();
+    handleEditItemClicked(); 
   }
 
   return {
-    getItemIdFromElement: getItemIdFromElement, 
-    render: render,
-    bindEventListeners: bindEventListeners
+    getItemIdFromElement, 
+    render,
+    bindEventListeners, 
   };
 
 })();
